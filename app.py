715feb59487750340040783a344007bddfbd710e
Sreecharan1234 <sreecharan9484@gmail.com>
2025-05-02 13:31:42 +0530

    this

--- app.py
+++ app.py
@@ -412,7 +412,7 @@ if selected == "Road Map":
     with col1:
         st.lottie(l, height=100, width=100)
     with col2:
-        st.header(f":rainbow[Get Your Personalised Roadmap]😎🧑‍🏫", divider='rainbow')
+        st.header(f":rainbow[Get Your Personalised  Road Map]😎🧑‍🏫", divider='rainbow')
     with st.form(key='survey_form'):
         col1, col2 = st.columns(2)  # Create two columns
         with col1:
